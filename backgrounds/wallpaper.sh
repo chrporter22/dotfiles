f6cb3d5125e824a46a970d0ab49c4b3d22e3e8a0
Christian Porter <cporter1993@gmail.com>
2025-06-23 02:11:13 +0000

    update backgrounds

--- backgrounds/wallpaper.sh
+++ backgrounds/wallpaper.sh
@@ -2,6 +2,8 @@
 # feh --bg-scale /home/cporter/.config/backgrounds/orig-dracula.png
 
 # feh --bg-scale /home/cporter/.config/backgrounds/dracula-arch.jpg
-feh --bg-scale /home/cporter/.config/backgrounds/nice-blue-background.png
+# feh --bg-scale /home/cporter/.config/backgrounds/nice-blue-background.png
 
 # feh --bg-scale /home/cporter/.config/backgrounds/wallpaper.jpg
+
+feh --bg-tile /home/cporter/.config/backgrounds/IMG_6622.JPG

6b17cddf14dad01a59d7630bb163a31a85cf7c2f
Christian Porter <cporter1993@gmail.com>
2025-07-29 19:31:42 -0400

    update vundle git repo path and clone location

--- data_sci_install.sh
+++ data_sci_install.sh
@@ -165,7 +165,7 @@ install_plugins() {
 
     # Vundle (classic Vim plugin manager)
     if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
-        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+        git clone https://github.com/VundleVim/Vundle.vim "$HOME/.vim/bundle/Vundle.vim"
     else
         echo "Vundle already installed, skipping."
     fi
